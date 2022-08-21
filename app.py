103e25124381aaac39882efeddee02da52764dea
minerpix <105188990+minerpix@users.noreply.github.com>
2022-08-21 15:48:51 -0700

    first commit

---
+++ app.py
@@ -0,0 +1,26 @@
+import flask
+
+# creating new flask app
+app = flask.Flask(__name__)
+
+#function declarator: routes to "/"
+@app.route('/')
+def home():
+    return flask.send_file('static/index.html')
+
+@app.route('/about')
+def about():
+    return flask.send_file('static/about.html')
+
+@app.route('/credits')
+def credits():
+    return flask.send_file('static/credits.html')
+
+@app.route('/vars/<num>')
+def routenumber(num):
+    output = "You chose the number " + str(num) + ", which is 1 more than " + str(int(num) - 1)
+    output += "<br><a href= '/vars'>Return</a>"
+    return output
+
+if __name__ == "__main__":
+    app.run()
